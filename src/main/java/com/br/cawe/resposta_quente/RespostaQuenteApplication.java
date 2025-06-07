8363aa62d9184ed225d97899d2dc556582a8df6c
CaweAlves <contato@cawe.dev>
2025-06-07 16:09:06 -0300

    init

---
+++ src/main/java/com/br/cawe/resposta_quente/RespostaQuenteApplication.java
@@ -0,0 +1,13 @@
+package com.br.cawe.resposta_quente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespostaQuenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespostaQuenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/cawe/resposta_quente/RespostaQuenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cawe.resposta_quente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespostaQuenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
